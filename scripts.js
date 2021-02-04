b9f1cfa85b64d5bdb54cd9257e4f92072906079d
carlos <carlos@freeline.inf.br>
2021-02-04 15:48:52 -0300

    adicionando a aula 3, começando

---
+++ scripts.js
@@ -0,0 +1,28 @@
+Modal = {
+    toggle(){
+        //Abrir Modal
+        //Adicionar a class active ao modal
+        //alert("Abri o Modal!!")
+        document.querySelector('.modal-overlay')
+        .classList.toggle('active');
+    // },
+    // close(){
+    //     //Fechar o Modal
+    //  //remover a class active do modal
+    //  document.querySelector('.modal-overlay')
+    //     .classList.remove('active');
+    }
+}
+
+//Eu preciso somar as entradas
+//depois eu preciso somar as saídas e
+//remover das entradas o valor das saídas
+
+const Transaction = {
+    incomes() {
+        //somar as entradas
+    },
+    expenses(){
+        //somar as saidas
+    }
+}
